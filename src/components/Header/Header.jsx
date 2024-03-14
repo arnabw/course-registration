23a250d3128f13e69d24431cc764ffcfcda464b8
arnabw <arnabsahawrk@gmail.com>
2024-03-14 16:31:07 +0600

    Main content component added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="px-4 md:px-10 lg:px-[50px] py-10">
+    <header className="px-4 md:px-10 lg:px-[50px] py-5 lg:py-10">
       <nav className="text-center">
         <h1 className="text-[#1C1B1B] text-2xl md:text-[32px] font-bold">
           Course Registration
