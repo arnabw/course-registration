131754d6951f8ccfd79c950888568d77217f905f
arnabw <arnabsahawrk@gmail.com>
2024-03-14 13:38:45 +0600

    Header component added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,13 @@
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className="px-4 md:px-10 lg:px-[50px] py-10">
+      <nav className="text-center">
+        <h1 className="text-[#1C1B1B] text-2xl md:text-[32px] font-bold">
+          Course Registration
+        </h1>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
