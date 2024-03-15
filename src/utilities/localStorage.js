019d2638ae5f21f27746166595d211ff507c0450
arnabw <arnabsahawrk@gmail.com>
2024-03-15 14:49:32 +0600

    local storage added

---
+++ src/utilities/localStorage.js
@@ -0,0 +1,54 @@
+//Get From Local Storage
+const getStorage = () => {
+  //Course Stored
+  let courseStored = window.localStorage.getItem("course");
+  courseStored
+    ? (courseStored = JSON.parse(courseStored))
+    : (courseStored = []);
+
+  //Total Hour Stored
+  let creditStored = window.localStorage.getItem("credit");
+  if (!creditStored) creditStored = 0;
+
+  //Total Price Stored
+  let priceStored = window.localStorage.getItem("price");
+  if (!priceStored) priceStored = 0;
+
+  return [courseStored, creditStored, priceStored];
+};
+
+//Save In Local Storage
+const saveStorage = (courseStored, creditStored, priceStored) => {
+  window.localStorage.setItem("course", JSON.stringify(courseStored));
+  window.localStorage.setItem("credit", creditStored);
+  window.localStorage.setItem("price", priceStored);
+};
+
+//Set In Local Storage
+const setStorage = (id, credit, price) => {
+  let [courseStored, creditStored, priceStored] = getStorage();
+
+  courseStored.push(id);
+  creditStored = credit;
+  priceStored = price;
+
+  saveStorage(courseStored, creditStored, priceStored);
+};
+
+//Clear Local Storage
+const clearStorage = () => {
+  window.localStorage.clear();
+};
+
+//Remove From Local Storage
+const removeStorage = (id, credit, price) => {
+  let [courseStored, creditStored, priceStored] = getStorage();
+
+  let remainingCourse = courseStored.filter((idx) => idx !== id);
+  creditStored = credit;
+  priceStored = price;
+
+  saveStorage(remainingCourse, creditStored, priceStored);
+};
+
+export { setStorage, clearStorage, removeStorage };
